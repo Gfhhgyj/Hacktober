fe81101ee31e64f25c00839d8aca1d2b4707ac6f
Oleksandr Kunich <Oleksandr_Kunich@epam.com>
2021-10-30 19:15:21 +0300

    ping pong game, coding for kids

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+import sys
+
+pygame.init()
+pygame.display.set_caption('ping pong')
+surface = pygame.display.set_mode((850,450))
+background = pygame.Color(90, 160, 130) 
+surface.fill(background) 
+pygame.display.update()        
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
---
+++ while.py
@@ -0,0 +1,13 @@
+# i = 9
+
+# while i>0:
+#     print(i)
+#     i=i-1
+
+sunduk = (5,0, 3,4,3,4,5)
+
+for d in sunduk:
+    if d == 4:
+        exit()
+    else:
+        print(d)
