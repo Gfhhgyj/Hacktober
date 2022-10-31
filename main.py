95698b7369524adbdba062d8fcee65b267c14c4e
gdkdiyxo <85572525+gdkdiyxo@users.noreply.github.com>
2022-10-31 17:46:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 pygame.init()
 pygame.display.set_caption('ding pong')
 surface = pygame.display.set_mode((850,450))
-background = pygame.Color(600, 1200, 1800) 
+background = pygame.Color(80, 160, 240) 
 surface.fill(background) 
 pygame.display.update()        
 
